55ed35fd2006a38fe52a9f3d49a952a4e7e1e386
Kresna Satya <halo@kresna.me>
2025-03-12 17:11:28 +0800

    refactor: remove _dochecks()
    The PHP built-in function inside _dochecks() already remove in PHP 8.0

--- src/Fpdf.php
+++ src/Fpdf.php
@@ -77,8 +77,6 @@ var $PDFVersion;         // PDF version number
 *******************************************************************************/
 function __construct($orientation='P', $unit='mm', $size='A4')
 {
-	// Some checks
-	$this->_dochecks();
 	// Initialization of properties
 	$this->page = 0;
 	$this->n = 2;
@@ -1044,18 +1042,6 @@ function Output($name='', $dest='')
 *                              Protected methods                               *
 *                                                                              *
 *******************************************************************************/
-function _dochecks()
-{
-	// Check availability of %F
-	if(sprintf('%.1F',1.0)!='1.0')
-		$this->Error('This version of PHP is not supported');
-	// Check mbstring overloading
-	if(ini_get('mbstring.func_overload') & 2)
-		$this->Error('mbstring overloading must be disabled');
-	// Ensure runtime magic quotes are disabled
-	if(get_magic_quotes_runtime())
-		@set_magic_quotes_runtime(0);
-}
 
 function _checkoutput()
 {
